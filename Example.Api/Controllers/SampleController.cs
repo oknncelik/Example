ca79b8f290ba8eaf1cabc17fc24ef8716a4c91e4
Okan Celik <okan.celik@outlook.com>
2021-01-18 18:45:54 +0300

    Sample Endpoint added.

---
+++ Example.Api/Controllers/SampleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Example.Api.Controllers
+{
+    [Authorize]
+    [Route("api/sample")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(new { Status = "Ok." });
+        }
+    }
+}
