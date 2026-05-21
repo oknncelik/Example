1a0417fded18c966a7148db057fa312c4b240313
Okan Çelik <okan.celik@outlook.com>
2026-05-21 22:57:20 +0300

    DependencyResolvers taki bağımlılıklar otomatik eklenecek hale getirildi.

--- Example.Core/DependencyResolvers/AutofacModule.cs
+++ Example.Core/DependencyResolvers/AutofacModule.cs
@@ -1,15 +1,15 @@
 ﻿#region
 
+using System.Reflection;
 using Autofac;
 using Autofac.Extras.DynamicProxy;
 using Castle.DynamicProxy;
-using Example.Business.Abstract;
 using Example.Business.Concreate;
 using Example.Common.Intercepters;
 using Example.Common.Security.Jwt;
 using Example.Common.Security.Jwt.Abstract;
-using Example.Dal.Abstract.Repositories;
 using Example.Dal.Concreate.Repositories;
+using Module = Autofac.Module;
 
 #endregion
 
@@ -19,44 +19,18 @@ namespace Example.Core.DependencyResolvers
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<TokenHelper>().As<ITokenHelper>()
-                .EnableInterfaceInterceptors(new ProxyGenerationOptions {Selector = new AspectInterceptorSelector()});
+            builder.RegisterType<TokenHelper>().As<ITokenHelper>().InstancePerLifetimeScope();
 
-            #region Repositories
+            var businessAssembly = Assembly.GetAssembly(typeof(AuthManager));
+            var dataAccessAssembly = Assembly.GetAssembly(typeof(UserRepository));
 
-            builder.RegisterType<UserRepository>().As<IUserRepository>()
-                .EnableInterfaceInterceptors(new ProxyGenerationOptions {Selector = new AspectInterceptorSelector()});
-            builder.RegisterType<OperationClaimRepository>().As<IOperationClaimRepository>()
-                .EnableInterfaceInterceptors(new ProxyGenerationOptions {Selector = new AspectInterceptorSelector()});
-            builder.RegisterType<UserOperationClaimRepository>().As<IUserOperationClaimRepository>()
-                .EnableInterfaceInterceptors(new ProxyGenerationOptions {Selector = new AspectInterceptorSelector()});
-            builder.RegisterType<CategoryRepository>().As<ICategoryRepository>()
-                .EnableInterfaceInterceptors(new ProxyGenerationOptions {Selector = new AspectInterceptorSelector()});
-            builder.RegisterType<ProductRepository>().As<IProductRepository>()
-                .EnableInterfaceInterceptors(new ProxyGenerationOptions {Selector = new AspectInterceptorSelector()});
-
-            #endregion
-
-            #region Managers
-
-            builder.RegisterType<AuthManager>().As<IAuthManager>()
-                .EnableInterfaceInterceptors(new ProxyGenerationOptions {Selector = new AspectInterceptorSelector()});
-            builder.RegisterType<CategoryManager>().As<ICategoryManager>()
-                .EnableInterfaceInterceptors(new ProxyGenerationOptions {Selector = new AspectInterceptorSelector()});
-            builder.RegisterType<ProductManager>().As<IProductManager>()
-                .EnableInterfaceInterceptors(new ProxyGenerationOptions {Selector = new AspectInterceptorSelector()});
-            builder.RegisterType<UserOperationClaimManager>().As<IUserOperationClaimManager>()
-                .EnableInterfaceInterceptors(new ProxyGenerationOptions {Selector = new AspectInterceptorSelector()});
-
-            #endregion
-
-            /*
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces().EnableInterfaceInterceptors(new ProxyGenerationOptions()
-            {
-                Selector = new AspectInterceptorSelector()
-            }).SingleInstance();    
-            */
+            builder.RegisterAssemblyTypes(businessAssembly, dataAccessAssembly)
+                .AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions
+                {
+                    Selector = new AspectInterceptorSelector()
+                })
+                .InstancePerLifetimeScope();
         }
     }
 }
